8530c7365066607de33285098a42a9f89e35a2e1
Neil <s20063196@163.com>
2018-12-10 14:07:01 +0800

    fix issue with cut.py doesn't work for all html files
    Signed-off-by: Neil <s20063196@163.com>

--- orgs/cut.py
+++ orgs/cut.py
@@ -1,8 +1,30 @@
 #!/bin/env/python3
 
 from bs4 import BeautifulSoup
+import sys
+import os
 
-soup = BeautifulSoup(open('openwrt-00013.html'), "lxml")
+
+def UsageExit():
+    print('python3 cut.py [.html file]')
+    sys.exit()
+
+
+if len(sys.argv) != 2:
+    UsageExit()
+
+if os.path.isfile(sys.argv[1]) == False:
+    UsageExit()
+
+soup = BeautifulSoup(open(sys.argv[1]), "lxml")
 
 k = soup.find('div', id='content')
+
+savedStdout = sys.stdout
+f = open("tmp.html", "w")
+sys.stdout = f
 print(k)
+f.close
+
+sys.stdout = savedStdout
+print("done")
